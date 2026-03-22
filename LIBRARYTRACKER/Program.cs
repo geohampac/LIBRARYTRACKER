ec9b311b18d09d22684216a5cbca41f8fb549835
Gerry Hampac <Gerry Hampac@LAPTOP-M7I4JAL5>
2026-03-22 21:52:32 +0800

    Add project files.

---
+++ LIBRARYTRACKER/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using LIBRARYTRACKER;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+// ? MudBlazor
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
